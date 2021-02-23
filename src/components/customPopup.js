72e0a07bc287195acab9335f1423e5ecac954c12
Stefan Begerad <sbegerad@posteo.de>
2021-02-23 13:15:51 -0500

    cleaned up Test map

---
+++ src/components/customPopup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Popup} from 'react-leaflet';
+
+const CustomPopup=()=>{
+    return(
+        <Popup>
+            This is a custom popup.
+        </Popup>
+    );
+}
+
+export default CustomPopup;
